c954004aaa8656dc5937643be549efdd24a0638b
Amanda <amanda.su@cooper.edu>
2025-03-29 12:25:13 -0400

    can quit start screen now

--- start_screen.py
+++ start_screen.py
@@ -20,49 +20,39 @@ BLACK = (0,0,0)
 font = pygame.font.Font('./start_screen/Domotika-Pro-Light-trial.ttf', 32)
 
 #Here the "click to start" is made a surface
-start1_text = font.render('Click Anywhere To Start', True, BLACK)
-start1_text_rect = start1_text.get_rect()
-start1_text_rect.midtop = (WINDOW_WIDTH // 2 - 6, 454)
+start1_surface = font.render('Click Anywhere To Start', True, BLACK)
 
-# #I added the images used for the title screen and placed them on the screen
-start_image = pygame.image.load('./start_screen/start_image.png')
-start_rect = start_image.get_rect()
-start_rect.midleft = (320, WINDOW_HEIGHT // 2)
-
-title_image = pygame.image.load('./start_screen/title.jpg')
-title_rect = title_image.get_rect()
-title_rect.midleft = (380, 355)
+#I added the images used for the title screen and placed them on the screen
+start_surface = pygame.image.load('./start_screen/start_image.png')
+title_surface = pygame.image.load('./start_screen/title.jpg')
 
 
-#I set up the background music
-# pygame.mixer.music.play(-1, 0.0)
-test_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
 screen.fill(WHITE)
+test_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+test_surface.fill(BLACK)
+
 
 running = True
-while running:
+start_screen = True
+
+while running:    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #The start screen is loaded first, but when the mouse is pressed, it moves to the next display
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            start_screen = False
 
-    #The start screen is loaded first, but when the mouse is pressed, it moves to the next display
-    start_screen = True
-    while start_screen:
+    if start_screen:
         screen.blit(screen,(0,0))
-        screen.blit(start_image, start_rect)
-        screen.blit(title_image, title_rect)
-        screen.blit(start1_text, start1_text_rect)
-        pygame.display.update()
-
-        for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                     start_screen = False
+        screen.blit(start_surface, (320, 160))
+        screen.blit(title_surface, (380, 300))
+        screen.blit(start1_surface, (465, 430))
                      
-    while not start_screen:
+    else:
         screen.blit(test_surface, (0,0))
-        pygame.display.update()
-
     
     clock.tick(60)
+    pygame.display.update()
 
 pygame.quit()
